fef8d4deb5c6e117993acb4e0fbeaf4480b94fc9
Gareth Daunton <gxd5116@rit.edu>
2015-09-28 15:16:52 -0400

    Started on some of the model code

---
+++ src/model/Account.java
@@ -0,0 +1,116 @@
+package model;
+
+import java.lang.Exception;
+import java.util.Calendar;
+import java.util.Date;
+
+public abstract class Account{
+
+    public enum Type{
+        BANK,
+        MONEY_MARKET
+    };
+
+    private String bankName;
+    private double balance;
+    private Date opened;
+    private Type type;
+
+    /**
+     * Creates a new Account object
+     * @param bankName The name of the bank
+     * @param balance The initial balance in the account
+     * @param type The type of the account
+     */
+    public Account(String bankName, double balance, model.Account.Type type) {
+        this.bankName = bankName;
+        this.balance = balance;
+        this.type = type;
+        opened = Calendar.getInstance().getTime();
+    }
+
+    /**
+     * @return The name of the bank
+     */
+    public String getBankName() {
+        return bankName;
+    }
+
+    /**
+     * Get the account balance
+     * @return The account balance
+     */
+    public double getBalance() {
+        return balance;
+    }
+
+    /**
+     * Withdraws the given ammount from the account and returns the remaining balance
+     * @param ammount The ammount to withdraw
+     * @return The remaining account balance
+     * @throws NegativeValueException
+     * @throws OverDrawException
+     */
+    public double withdraw(double ammount) throws NegativeValueException, OverDrawException{
+        if(ammount < 0)
+            throw NegativeValueException("Please give a positive value");
+        if(balance - ammount < 0)
+            throw OverDrawException("You cannot overdraw your account");
+        return this.balance -= ammount;
+    }
+
+    /**
+     * Deposits the given ammount from the account and returns the new balance
+     * @param ammount The ammount to deposit
+     * @return The new account balance
+     * @throws NegativeValueException
+     */
+    public double deposit(double ammount) throws NegativeValueException{
+        if(ammount < 0)
+            throw NegativeValueException("Please give a positive value");
+        return this.balance += ammount;
+    }
+
+    /**
+     * Transfers the given ammount to the new account
+     * @param ammount The ammount to transfer
+     * @param accout The account to transfer the ammount to
+     * @return The remaining account balance
+     * @throws NullAccountException
+     * @throws NegativeValueException
+     * @throws OverDrawException
+     */
+    public double transfer(double ammount, Account accout) throws NullAccountException, NegativeValueException, OverDrawException{
+        if(account == null)
+            throw NullAccountException("Please give a valid account");
+        if(ammount < 0)
+            throw NegativeValueException("Please give a positive value");
+        if(balance - ammount < 0)
+            throw OverDrawException("You cannot overdraw your account");
+        this.withdraw(ammount);
+        account.deposit(ammount);
+        return this.getBalance();
+    }
+
+    /**
+     * Get the type of the account
+     * @return The type of the account
+     */
+    public Type getType() {
+        return type;
+    }
+
+    /**
+     * Get the date the account was opened
+     * @return The date the account was opened
+     */
+    public Date getOpened() {
+        return opened;
+    }
+
+    public class OverDrawException extends Exception{}
+
+    public class NegativeValueException extends Exception{}
+
+    public class NullAccountException extends Exception{}
+}
---
+++ src/model/Equity.java
@@ -0,0 +1,96 @@
+package model;
+
+public abstract class Equity{
+
+    public enum Type{
+        STOCK,
+        BOND,
+        MUTUAL_FUND
+    };
+
+    private String tickerSymbol;
+    private String name;
+    private int shares;
+    private double price_per_share;
+    private double totalValue;
+    private int marketIndex;
+    private String industrySector;
+    private Type type;
+
+    /**
+     * Creates a new Equity Object
+     * @param type The type of the Equity
+     * @param tickerSymbol the Ticker Symbol for the Equity
+     * @param name The name of the Equity
+     * @param shares The ammount of shares in the Equity
+     * @param price_per_share The price of each share of the Equity
+     */
+    public Equity(Type type, String tickerSymbol, String name, int shares, double price_per_share) {
+        this.tickerSymbol = tickerSymbol;
+        this.name = name;
+        this.shares = shares;
+        this.price_per_share = price_per_share;
+        this.totalValue = this.shares * this.price_per_share;
+        this.type = type;
+    }
+
+    /**
+     * Get the ticker symbol
+     * @return The ticker symbol
+     */
+    public String getTickerSymbol(){
+        return tickerSymbol;
+    }
+
+    /**
+     * Get the name of the Equity
+     * @return The name of the Equity
+     */
+    public String getName(){
+        return name;
+    }
+
+    /**
+     * Get the number of shares in the Equity
+     * @return The number of shares in the Equity
+     */
+    public int getShares(){
+        return shares;
+    }
+
+    /**
+     * Set the number of shares in the Equity
+     * @param shares The new ammount of shares in the Equity
+     */
+    public void setShares(int shares) {
+        this.shares = shares;
+    }
+
+    /**
+     * Get the price per share of the Equity
+     * @return The price per share of the Equity
+     */
+    public double getPrice_per_share() {
+        return price_per_share;
+    }
+
+    /**
+     * Set the price per share of the Equity
+     * @param price_per_share The new price per share of the Equity
+     */
+    public void setPrice_per_share(double price_per_share) {
+        this.price_per_share = price_per_share;
+    }
+
+    /**
+     * Get the total value of the Equity
+     * @return The total value of the Equity
+     */
+    public double getTotalValue() {
+        return totalValue;
+    }
+
+    abstract void add{}
+
+    abstract void remove{}
+}
