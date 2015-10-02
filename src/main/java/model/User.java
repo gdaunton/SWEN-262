369377523178321a82bae6c8f1da76b865bf26f5
Gareth Daunton <gdaunton@gmail.com>
2015-10-02 11:31:34 -0400

    Switched to storing all of the users in a file instead of memory.

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,36 +1,18 @@
 package model;
 
-import model.util.HashSlingingSlasher;
+import java.io.Serializable;
 
-public class User{
-    private String username;
-    private String password_hash;
+public class User implements Serializable {
+    public String username;
+    public String passwordHash;
+    private int id;
 
-    /**
-     * Creates a new User object
-     * @param username The user's username
-     * @param password_hash The user's password hash
-     */
-    public User(String username, String password_hash){
+    public User(String username, String hash, int id){
         this.username = username;
-        this.password_hash = password_hash;
+        this.passwordHash = hash;
     }
 
-    /**
-     * Gets the user's username
-     * @return The user's username
-     */
-    public String getUsername(){
-        return username;
-    }
-
-    /**
-     * Compaires the given password with this user's hash
-     * @param password The password to check
-     * @return Whether the password matches this user's hash
-     * @throws Exception
-     */
-    public boolean compareHash(String password) throws Exception{
-        return HashSlingingSlasher.check(password, this.password_hash);
+    public int getId(){
+        return this.id;
     }
 }
