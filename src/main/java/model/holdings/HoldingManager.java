b5f60a626ec4900bafce04a17bbeed579a1b3746
Joe Klesczewski <jklesczewski@gmail.com>
2015-10-07 18:28:46 -0400

    More HoldingManager stuff

--- src/main/java/model/holdings/HoldingManager.java
+++ src/main/java/model/holdings/HoldingManager.java
@@ -5,10 +5,8 @@ import java.util.ArrayList;
 
 public class HoldingManager {
     static HashMap<Portfolio, ArrayList<Holding>> owned_holding_lists = new HashMap<Portfolio, Holding>();
-    public static ArrayList<Holding> holdings = null;
+    public static ArrayList<Equity> equity_list = null;
     
     public static void grant(Portfolio p, Holding h) { owned_holding_lists.get(p).add(h); }
     public static void revoke(Portfolio p, Holding h){ owned_holding_lists.get(p).remove(h); }
-    
-    
 }
