6996a50f5166ce8abc7734cab49e8bc5529fbfb4
Joe Klesczewski <jklesczewski@gmail.com>
2015-10-08 12:51:04 -0400

    Holding now Comparable

--- src/main/java/main/model/holdings/Holding.java
+++ src/main/java/main/model/holdings/Holding.java
@@ -2,6 +2,7 @@ package model.holdings
 
 import java.util.ArrayList;
 
-public abstract class Holding {
-    abstract String toString();
+public abstract class Holding implements Comparable {
+    abstract String[] as_table_row();
+    abstract int compareTo(Object o);
 }
