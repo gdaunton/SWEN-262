7198e89e4fedfadafc4491eff0429c9bb4d2647d
JoeKlesczewski <jklesczewski@gmail.com>
2015-10-21 08:24:09 -0400

    Pasted skeleton of the simulation controllers

---
+++ src/main/java/main/view/BearController.java
@@ -0,0 +1,102 @@
+package main.view;
+
+import java.net.URL;
+import java.text.NumberFormat;
+import java.text.SimpleDateFormat;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import main.controller.command.HoldingCommand;
+import main.model.Portfolio;
+import main.view.MainController;
+
+
+public class BearController {
+
+	//buttons
+	@FXML
+	private Button run;
+	@FXML
+	private Button restart;
+	
+	//configuration
+		//dropdown for choosing the simulation length
+	
+	//rate of growth? would be a textfield
+	
+	//portfolio summation stuff
+	
+
+	//objects
+	private Portfolio p;
+	private Portfolio op;
+	private MainController controller;
+	private Bear s = new Bear();
+
+	/**
+	 * Sets the record.
+	 * 
+	 * @param controller
+	 *            The controller.
+	 * @param p
+	 *            The portfolio.
+	 */
+	public void setTransaction(MainController controller, Portfolio p) {
+		this.p = p.clone();
+		this.op = p;
+		this.controller = controller;
+		initValues();
+	}
+
+	/**
+	 * Initializes the record controller.
+	 * 
+	 * @param location
+	 *            The location.
+	 * @param resources
+	 *            The resources.
+	 */
+	public void initialize(URL location, ResourceBundle resources) {
+		assert run != null : "fx:id=\"run\" was not injected: check your FXML file.";
+		run.setOnAction(new EventHandler<ActionEvent>() {
+			public void handle(ActionEvent event) {
+				do_simulate();
+			}
+		});
+
+		assert restart != null : "fx:id=\"restart\" was not injected: check your FXML file.";
+		restart.setOnAction(new EventHandler<ActionEvent>() {
+			public void handle(ActionEvent event) {
+				p = p.clone();
+				updateView();
+			}
+		});
+	}
+	
+	public void do_simulate() {
+		p = s.simulate(steps(), step_size(), p);
+	}
+
+	/**
+	 * Sets the initial values of the components.
+	 */
+	private void initValues() {
+		try {
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Reset the values of the components.
+	 */
+	public void updateView() {
+		try {
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
